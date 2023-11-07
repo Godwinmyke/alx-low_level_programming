f75ce97badd8f105e75307439b70c897610726ed
Godwin <godwinmyke@gmail.com>
2023-11-06 18:14:28 -0800

    C2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main funcion
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
