27f3061cb36d14e6c57bb24cb59e743b5f81c054
Godwin <godwinmyke@gmail.com>
2023-11-09 15:32:09 -0800

    Sizd of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * retuen: always 0
+ */
+int main (void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(char));
+	return(0);
