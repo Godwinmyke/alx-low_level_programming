74e7ed01bf612683a46474e6cfedb917983ffb2b
Godwin <godwinmyke@gmail.com>
2023-11-09 16:40:02 -0800

    o

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - This is the main function
+ * Return: 1 (Success)
+int main(void)
+{
+    if (write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) == -1)
+        return 1;
+
+    return 1;
+}
