e9f3b76cd87ff3a78deba7e306412596bd01adb8
Godwin <godwinmyke@gmail.com>
2023-11-09 16:41:31 -0800

    o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 /**
  * main - This is the main function
  * Return: 1 (Success)
+ */
 int main(void)
 {
     if (write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) == -1)
