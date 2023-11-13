e1e3b3675b24ad839a4d442f9b73b14a6c80a186
Godwin <godwinmyke@gmail.com>
2023-11-12 17:03:24 -0800

    a2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0 ; a <= 8 ; a++)
+	for (a = 0 ; a < 9 ; a++)
 	{
-	for (b = a + 1 ; b <= 9 ; b++)
+	for (b = a + 1 ; b < 10 ; b++)
 	{
 	putchar(a + '0');
 	putchar(b + '0');
