9424276e878ed13bdc8b51b09dfdf20513a9f772
Godwin <godwinmyke@gmail.com>
2023-11-25 15:36:56 -0800

    Focus

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - this is the _isalpha function
+ * @c: Paremeter to be checked
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
