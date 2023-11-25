e4ee24af33957f3fafd03b589515b0f5259f080d
Godwin <godwinmyke@gmail.com>
2023-11-25 10:08:12 -0800

    love

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - This is the print_alphabet function
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
