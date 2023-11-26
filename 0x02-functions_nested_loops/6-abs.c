57e89a9b5a55893a53b885cd545d272944796768
Godwin <godwinmyke@gmail.com>
2023-11-26 08:22:51 -0800

    God

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - This is the int _abs function
+ * @a: Paremeter
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (-a);
+	}
+	else
+		return (a);
+}
