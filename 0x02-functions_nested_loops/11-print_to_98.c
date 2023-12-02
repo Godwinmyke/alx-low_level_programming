23987003a3e5b422a3e5f13666d7fadf544a23b7
Godwin <godwinmyke@gmail.com>
2023-12-02 05:38:35 -0800

    print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - This is the print_to_98 function
+ * Return: Always 0
+ * @n: Paremeter
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	}
+}
