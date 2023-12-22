bac12bdb29694df5d9a6648e7f0f6c75b73f817b
Godwin <godwinmyke@gmail.com>
2023-12-21 18:13:09 -0800

    _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - This is the print_numbers function
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
