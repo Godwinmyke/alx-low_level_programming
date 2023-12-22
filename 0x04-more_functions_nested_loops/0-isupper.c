3131b8d81e2cfb54c946a3d31a14cbba978313ef
Godwin <godwinmyke@gmail.com>
2023-12-21 16:25:20 -0800

    Good

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isupper - This is the _isupper function
+*Return: Always 0
+*@c: paremeter
+*/
+int _isupper(int c)
+{
+	if (c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
