119e345a9a796267a5c92b66470a6c97138cdb8e
Godwin <godwinmyke@gmail.com>
2024-03-13 08:41:30 -0700

    loop

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - Function
+ * @s1: Parameter to be compare
+ * @s2: Parameter to be compare
+ * Return: Void
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+			return (0);
+}
