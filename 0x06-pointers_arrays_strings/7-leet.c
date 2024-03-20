92d65bc7383f9663e89f67a3437f421b887e7500
Godwin <godwinmyke@gmail.com>
2024-03-20 05:56:13 -0700

    ASCII

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - Function
+ * @str: Parameter
+ * Return: Void
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return (str);
+}
