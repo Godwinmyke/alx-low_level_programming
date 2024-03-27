0986b19ddf6362736cc47a164888f483f068d61f
Godwin <godwinmyke@gmail.com>
2024-03-27 06:20:35 -0700

    go

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Function
+ * @s: Parameter
+ * @c: Parameter
+ * Return: Void
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
